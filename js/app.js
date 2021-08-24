01b84c18047a3b90ae1c16255ca8336b63ccb51e
Vladimir Zecevic <zecevicvladimir1@gmail.com>
2021-08-24 12:08:47 +0200

    [ADD] - Project Page

--- js/app.js
+++ js/app.js
@@ -58,12 +58,49 @@ const homeNumbers = document.querySelectorAll('.home-numbers .num');
 
 homeNumbers.forEach((num) => {
   gsap.from(num, {
-    delay: 2, 
+    delay: 2,
     duration: 2.5,
     innerHTML: 0,
-    snap:{
+    snap: {
       innerHTML: 1
     },
     scrollTrigger: ".home-numbers .bottom .text"
   });
 });
+
+/* #Project Information 1
+  ======================================================= */
+if (document.querySelector('.project-information-1 .swiper-container')) {
+  new Swiper('.project-information-1 .swiper-container', {
+    pagination: {
+      el: '.project-information-1 .swiper-pagination',
+    },
+    navigation: {
+      nextEl: '.swiper-arrows .next',
+      prevEl: '.swiper-arrows .prev',
+    },
+    slidesPerView: 1.15
+  });
+}
+
+/* #Project Slider
+  ======================================================= */
+if (document.querySelector('.project-slider .swiper-container')) {
+  new Swiper('.project-slider .swiper-container', {
+    pagination: {
+      el: '.project-slider .swiper-pagination',
+    },
+    navigation: {
+      nextEl: '.swiper-arrows .next',
+      prevEl: '.swiper-arrows .prev',
+    },
+    breakpoints: {
+      0: {
+        slidesPerView: 1
+      },
+      1024: {
+        slidesPerView: 4
+      }
+    }
+  });
+}
